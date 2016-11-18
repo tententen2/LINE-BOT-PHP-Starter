c134efb3346bfafa25aa6299b7758cc4701c907c
tententen2 <ten303@hotmail.com>
2016-11-18 16:32:49 +0700

    0

--- index.php
+++ index.php
@@ -1,2 +1,14 @@
 <?php
-echo "Hello LINE BOT333";
+    $apiKey = 'AIzaSyAUSbhKyzq-fA6P0U_94vUaQjZRVZKJQBU';
+    $text = 'Hello world!';
+    $url = 'https://www.googleapis.com/language/translate/v2?key=' . $apiKey . '&q=' . rawurlencode($text) . '&source=en&target=fr';
+
+    $handle = curl_init($url);
+    curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($handle);                 
+    $responseDecoded = json_decode($response, true);
+    curl_close($handle);
+
+    echo 'Source: ' . $text . '<br>';
+    echo 'Translation: ' . $responseDecoded['data']['translations'][0]['translatedText'];
+?>
