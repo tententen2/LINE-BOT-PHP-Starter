d8d052eed201e016ac16b36c687e0bf3184908e9
tententen2 <ten303@hotmail.com>
2016-11-18 15:45:24 +0700

    ่้่ม

--- bot.php
+++ bot.php
@@ -19,7 +19,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => '222'
+				'text' => $text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
