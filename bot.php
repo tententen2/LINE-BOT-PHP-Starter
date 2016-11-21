c0340688b0b2a718df90ca30f2893a069d939920
tententen2 <ten303@hotmail.com>
2016-11-21 14:59:24 +0700

    dad

--- bot.php
+++ bot.php
@@ -71,7 +71,7 @@ if (!is_null($events['events'])) {
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => array({$messages},{$image}),
+				'messages' => [$image],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
