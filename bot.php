c3b194a2ea5f5fd9c9403eb9aa0962cbe77dc601
tententen2 <ten303@hotmail.com>
2016-11-21 14:55:26 +0700

    ng

--- bot.php
+++ bot.php
@@ -71,7 +71,7 @@ if (!is_null($events['events'])) {
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$image],
+				'messages' => [{$messages},{$image}],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
