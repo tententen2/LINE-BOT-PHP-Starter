dde54a26b313f869fe0f43aad8eabc8a751661ee
tententen2 <ten303@hotmail.com>
2016-11-21 16:50:16 +0700

    wd

--- bot.php
+++ bot.php
@@ -101,7 +101,7 @@ if (!is_null($events['events'])) {
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => array($jsondata),
+				'messages' => array([$jsondata]),
 			];
 			
 			// $data1 = [
