02f353a830b57047d854566e8390e773f83e85bc
tententen2 <ten303@hotmail.com>
2016-11-21 14:42:53 +0700

    da

--- bot.php
+++ bot.php
@@ -63,8 +63,8 @@ if (!is_null($events['events'])) {
 
 			$image = [
 				'type' => 'image',
-				'originalContentUrl' => "http://f.ptcdn.info/464/041/000/o505xu5rsDUycpMYem9-o.jpg",
-				'previewImageUrl' => 'http://sv6.postjung.com/picpost/data/184/184340-1-2995.jpg'
+				'originalContentUrl' => "'http://f.ptcdn.info/464/041/000/o505xu5rsDUycpMYem9-o.jpg'",
+				'previewImageUrl' => "'http://sv6.postjung.com/picpost/data/184/184340-1-2995.jpg'"
 
 			];
 			// Make a POST Request to Messaging API to reply to sender
