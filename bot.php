90dc2f1eae6de75b66b95f2e70118d429b32393c
tententen2 <ten303@hotmail.com>
2016-11-22 10:35:26 +0700

    หก

--- bot.php
+++ bot.php
@@ -50,7 +50,7 @@ if (!is_null($events['events'])) {
 
 				$jsondata = [
 					'type' => 'text',
-					'text' => "ผลการค้นหา :"."\r\n\r\n".$result_text
+					'text' => "ผลการค้นหา :"."\r\n".$result_text
 				];
 
 			}else if($text_split[0] == "ประมูล"){
