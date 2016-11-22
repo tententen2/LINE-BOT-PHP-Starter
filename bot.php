4fa77e945c6438ca7dc7a442c201b08b90762e6d
tententen2 <ten303@hotmail.com>
2016-11-22 09:51:17 +0700

    da

--- bot.php
+++ bot.php
@@ -77,7 +77,7 @@ if (!is_null($events['events'])) {
 					"type" => "buttons",
 					"thumbnailImageUrl" => $urlimg,
 					"title" => $name,
-					"text" => "เริ่มประมูลวันที่ ".substr($ff,0,10)."\r\n"."เวลา ".substr($ff,11,18)."\r\n".'บิตครั้งละ '.$money.' บาท !!'."\r\n\r\n".'ไปลงทะเบียนกันเลย ><',
+					"text" => "เริ่มประมูลวันที่ ".substr($ff,0,10)."\r\n"."เวลา ".substr($ff,11,18)."\r\n".'บิตครั้งละ '.$money.' บาท !!',
 					"actions" => [
 						
 					]
