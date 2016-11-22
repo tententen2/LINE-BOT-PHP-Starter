a05c7411a6a81ce3913d2237046cd715a151deb8
tententen2 <ten303@hotmail.com>
2016-11-22 11:26:37 +0700

    few

--- bot.php
+++ bot.php
@@ -65,7 +65,8 @@ if (!is_null($events['events'])) {
     				foreach($jsons as $key => $value) {
     					foreach($value as $key => $kk){
     							if($key == 'detail'){
-    								if(preg_match('/\w*('.$text_split[1].')+/', $kk)){
+    								$fkfk = '/\w*('.$text_split[1].')+/';
+    								if(preg_match($fkfk, $kk)){
     									$ff = $value['time'];
     									$money = $value['priceforbit'];
     									$name = $kk;
