ada4789ab335d146e10231e4de26f239f79620a5
tententen2 <ten303@hotmail.com>
2016-11-23 10:05:34 +0700

    c

--- bot.php
+++ bot.php
@@ -55,7 +55,7 @@ if (!is_null($events['events'])) {
 				if(empty($result_text)){
 					$result_text = 'ไม่พบข้อมูล';
 				}
-				$result_text = filter_var($result_text, FILTER_SANITIZE_URL);
+				$result_text = filter_var($result_text, FILTER_SANITIZE_STRING , FILTER_FLAG_STRIP_BACKTICK);
 				$jsondata = [
 					'type' => 'text',
 					'text' => 'ผลการค้นหา1 :'."\r\n"."\"".$result_text."\""
