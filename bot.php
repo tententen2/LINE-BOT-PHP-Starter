79e9e241d025aebee6c4b58646de9b677597d028
tententen2 <ten303@hotmail.com>
2016-11-23 09:39:59 +0700

    dd

--- bot.php
+++ bot.php
@@ -55,7 +55,7 @@ if (!is_null($events['events'])) {
 				if(empty($result_text)){
 					$result_text = 'ไม่พบข้อมูล';
 				}
-				$result_text = str_replace(array("\\n\r", "\\n", "\\r","\\"), "", $result_text);
+				$result_text = str_replace(array("\n\r", "\n", "\r","\\"), "", $result_text);
 				$jsondata = [
 					'type' => 'text',
 					'text' => 'ผลการค้นหา1 :'."\r\n"."\"".$result_text."\""
