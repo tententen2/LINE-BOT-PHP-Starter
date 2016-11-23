cfe85784ecd6ab9257cef2e24bcfefda7afb47f9
tententen2 <ten303@hotmail.com>
2016-11-23 10:10:52 +0700

    ca

--- bot.php
+++ bot.php
@@ -52,7 +52,7 @@ if (!is_null($events['events'])) {
 					}
 					
 				}
-				$result_text = filter_var($result_text, FILTER_SANITIZE_FULL_SPECIAL_CHARS);
+				$result_text = filter_var($result_text, FILTER_SANITIZE_SPECIAL_CHARS, FILTER_FLAG_STRIP_BACKTICK);
 				if(empty($result_text)){
 					$result_text = 'ไม่พบข้อมูล';
 				}
