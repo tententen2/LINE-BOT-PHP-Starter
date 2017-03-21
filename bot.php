68f7fd462ed1eb437a513985bc5fe5e10938599d
tententen2 <ten303@hotmail.com>
2017-03-21 19:53:20 +0700

    w

--- bot.php
+++ bot.php
@@ -91,27 +91,23 @@ if (!is_null($events['events'])) {
 						];
     				}else{
     					if(count($ff) == 1){
-							// 	$jsondata = [
-							// 	"type" => "template",
-							// 	"altText" => "this is a carousel template",
-							// 	"template" => [
-							// 	"type" => "carousel",
-							// 	"columns" => [
-							// 		[
-							// 		"thumbnailImageUrl" => $urlimg[0],
-							// 		"title" => $name[0],
-							// 		"text" => "เริ่มประมูลวันที่ ".substr($ff[0],0,10)."\r\n"."เวลา ".substr($ff[0],11,18)."\r\n".'บิตขั้นต่ำ '.$money[0].' บาท !!',
-							// 		"actions" => [
+								$jsondata = [
+								"type" => "template",
+								"altText" => "this is a carousel template",
+								"template" => [
+								"type" => "carousel",
+								"columns" => [
+									[
+									"thumbnailImageUrl" => $urlimg[0]."jgp",
+									"title" => $name[0],
+									"text" => "เริ่มประมูลวันที่ ".substr($ff[0],0,10)."\r\n"."เวลา ".substr($ff[0],11,18)."\r\n".'บิตขั้นต่ำ '.$money[0].' บาท !!',
+									"actions" => [
 						
-							// 			]
-							// 		]
-							// 	]
-							// 	]
-							// ];
-							$jsondata = [
-							'type' => 'text',
-							'text' => "ไม่มีการประมูล : ".$text_split[1].$urlimg[0]."HHHHH ".substr($ff[0],0,10)."FFFF ".substr($ff[0],11,18)
-						];
+										]
+									]
+								]
+								]
+							];
 
 						}else if(count($ff) == 2){
 							$jsondata = [
